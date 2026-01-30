0bf06c15b0b662ce875e2a3558b65426c4ed9f98
jzuss <evg.kch@gmail.com>
2026-01-30 20:07:18 +0300

    perfect ratelimit

--- ratelimit/ratelimit.go
+++ ratelimit/ratelimit.go
@@ -8,8 +8,9 @@ import (
 
 // Limiter is precise rate limiter with context support.
 type Limiter struct {
-	request chan chan error
-	signal  chan struct{}
+	tokens chan struct{}
+	stamps chan struct{}
+	signal chan struct{}
 }
 
 var ErrStopped = errors.New("limiter stopped")
@@ -18,80 +19,56 @@ var ErrStopped = errors.New("limiter stopped")
 // to maxCount events at any given interval.
 func NewLimiter(maxCount int, interval time.Duration) *Limiter {
 	l := &Limiter{
-		request: make(chan chan error),
-		signal:  make(chan struct{}),
+		tokens: make(chan struct{}, maxCount),
+		stamps: make(chan struct{}, maxCount),
+		signal: make(chan struct{}),
 	}
-	go l.loop(maxCount, interval)
+	for i := 0; i < maxCount; i++ {
+		l.tokens <- struct{}{}
+	}
+	go l.loop(interval)
 	return l
 }
 
-func (l *Limiter) loop(maxCount int, interval time.Duration) {
-	timeline := make([]time.Time, 0, maxCount)
-	var timer *time.Timer
+func (l *Limiter) loop(interval time.Duration) {
+	var scheduler []*time.Timer
+	var timeout <-chan time.Time
 
 	for {
-		var request chan chan error
-		var timeout <-chan time.Time
-
-		now := time.Now()
-
-		if len(timeline) < maxCount {
-			request = l.request
-		} else {
-			next := timeline[0].Add(interval)
-			if now.After(next) {
-				request = l.request
-			} else {
-				if timer == nil {
-					timer = time.NewTimer(next.Sub(now))
-				}
-				timeout = timer.C
-			}
+		if len(scheduler) > 0 {
+			timeout = scheduler[0].C
 		}
 
 		select {
+		case <-l.stamps:
+			scheduler = append(scheduler, time.NewTimer(interval))
+		case <-timeout:
+			scheduler = scheduler[1:]
+			l.tokens <- struct{}{}
 		case <-l.signal:
-			if timer != nil {
+			for _, timer := range scheduler {
 				timer.Stop()
 			}
-			for {
-				select {
-				case ch := <-l.request:
-					ch <- ErrStopped
-				default:
-					return
-				}
-			}
-
-		case <-timeout:
-			timer = nil
-
-		case response := <-request:
-			t := time.Now()
-			if len(timeline) < maxCount {
-				timeline = append(timeline, t)
-			} else {
-				timeline = append(timeline[1:], t)
-			}
-			response <- nil
+			return
 		}
 	}
 }
 
 func (l *Limiter) Acquire(ctx context.Context) error {
-	response := make(chan error, 1)
 
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
-	case l.request <- response:
-	}
-
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case err := <-response:
-		return err
+	case <-l.tokens:
+		l.stamps <- struct{}{}
+		select {
+		case <-l.signal:
+			return ErrStopped
+		default:
+			return nil
+		}
+	case <-l.signal:
+		return ErrStopped
 	}
 }
 
